83fb8123f9b53c588a1c0f4c71fddd7fc9980ad5
MoogieMoo <yxia1@stuy.edu>
2017-03-06 21:08:30 -0500

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -3,22 +3,7 @@
 // HW13--
 // 2017-03-06
 
-public class Tester {
-
-    //initiate array
-    int[] arr = new int[4];
-    //populate
-    for ( int i = 0; i < 4; i++ ) {
-	arr[i] = (int) ( Math. random() * 50 + 1 );//min=1, max=50
-    }
-
-    //initiate a, b, and c as random integers from 0 to 3
-    int a = (int)( Math. random() * 4 );
-    int b = (int)( Math. random() * 4 );
-    while ( b == 1 ) {//discovered in class that b should not be 1
-	b = (int)( Math. random() * 4 );
-    }
-    int c = (int)( Math. random() * 4 );
+public class Tester {    
 
     public static void swap ( int[] arr, int x, int y ) {
 	
@@ -47,6 +32,23 @@ public class Tester {
 
     public static void main ( String[] args ) {
 
-	Tester Tee = new Tester();
+	//initiate array
+	int[] arr = new int[4];
+	//populate
+	for ( int i = 0; i < 4; i++ ) {
+	    arr[i] = (int) ( Math. random() * 50 + 1 );//min=1, max=50
+	}
+
+	//initiate a, b, and c as random integers from 0 to 3
+	int a = (int)( Math. random() * 4 );
+	int b = (int)( Math. random() * 4 );
+	while ( b == 1 ) {//discovered in class that b should not be 1
+	    b = (int)( Math. random() * 4 );
+	}
+	int c = (int)( Math. random() * 4 );
+
+	mysterion( arr, a, b, c );
+	//System.out.println( mysterion( arr, a, b, c );
 
     }
+}

96b0b5e6f7263f40038ce3a70dca389f18ee82ce
MoogieMoo <yxia1@stuy.edu>
2017-03-06 20:49:58 -0500

    Add files via upload

---
+++ Tester.java
@@ -0,0 +1,52 @@
+// Maggie Xia
+// APCS2 pd1
+// HW13--
+// 2017-03-06
+
+public class Tester {
+
+    //initiate array
+    int[] arr = new int[4];
+    //populate
+    for ( int i = 0; i < 4; i++ ) {
+	arr[i] = (int) ( Math. random() * 50 + 1 );//min=1, max=50
+    }
+
+    //initiate a, b, and c as random integers from 0 to 3
+    int a = (int)( Math. random() * 4 );
+    int b = (int)( Math. random() * 4 );
+    while ( b == 1 ) {//discovered in class that b should not be 1
+	b = (int)( Math. random() * 4 );
+    }
+    int c = (int)( Math. random() * 4 );
+
+    public static void swap ( int[] arr, int x, int y ) {
+	
+	int h = arr[x];
+	arr[x] = arr[y];
+	arr[y] = h;
+	
+    }
+    
+    public int mysterion ( arr, a, b, c ) {
+	
+	int v = arr[c];
+	swap( arr, c, b );
+	int s = a;
+
+	for ( int i = a, i < b; i++ ) {
+	    if ( arr[i] < v ) {
+		swap( arr, s, i );
+		s += 1;
+	    }
+	}
+
+	swap( arr, b, s );
+	return s;
+    }
+
+    public static void main ( String[] args ) {
+
+	Tester Tee = new Tester();
+
+    }
